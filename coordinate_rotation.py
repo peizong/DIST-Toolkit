9aeac41196d6fa3b5d6631057a6b5584937e4e18
peizong <45988544+peizong@users.noreply.github.com>
2020-05-07 14:31:12 -0700

    Update coordinate_rotation.py

--- coordinate_rotation.py
+++ coordinate_rotation.py
@@ -20,7 +20,7 @@ import sys
 import random
 
 class coordinate_rotation():
-  """generate a dislocation"""
+  """rotation of coordinates"""
   def __init__(self,filename,filename_coord):
     self.filename=filename
     self.filename_coord=filename_coord
